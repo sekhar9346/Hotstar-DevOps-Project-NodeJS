40357da926b1717c55efbf85a8d578ca617ce6e9
sekhar9346 <anakalasekhar004@gmail.com>
2025-02-05 11:55:20 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ohdowdqc" # Replace with your actual S3 bucket name
+    bucket = "pasascxaqs" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "eu-west-1"
+    region = "us-east-1"
   }
 }
